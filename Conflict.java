bffb573079e2f3a0ead368d017f7c3b8e37187de
khoagit <khoa.dp2k@gmail.com>
2025-05-17 22:23:07 +0700

    commit master

--- Conflict.java
+++ Conflict.java
@@ -1,5 +1,8 @@
 public class Conflict {
     public void print(){
-        System.out.println("conflict main123");
+        System.out.println("conflict main12x");
+        System.out.println("conflict main123x");
+        System.out.println("conflict main12x");
+        System.out.println("conflict main123x");
     }
 }
